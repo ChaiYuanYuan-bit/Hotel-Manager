261d95527121eb85f1f6525826bda565067d9975
Coding Boy <chaiyuanyuan432@gmail.com>
2023-03-28 21:28:07 +0800

    update

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,9 @@
 //api地址
-export const baseURL = 'http://localhost:3000/api1/'
+let baseURL = 'http://localhost:3000/api1/'
+
+if (process.env.REACT_APP_TYPE === 'build') {
+    baseURL = 'http://localhost:5000/';
+}
+
+export default baseURL
+ 
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {baseURL} from '../config'
+import baseURL from '../config'
 
 const axiosInstance = axios.create({
     baseURL:baseURL,
